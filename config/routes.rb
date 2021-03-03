109707c917abad04203a33f52e83752a7b0202ea
denimcouch <alexmata92@ymail.com>
2021-03-03 12:00:28 -0600

    revised controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resource :users
+  namespace :api do
+    resources :users, only: [:index, :create, :show]
+    post "/login", to: "users#login"
+    get "/auto_login", to: "users#auto_login"
+
+  end
+  
 end
