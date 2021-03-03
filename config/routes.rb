f1e973aecd10088d75364f0bc52027a430dc8e37
denimcouch <alexmata92@ymail.com>
2021-03-03 12:11:22 -0600

    routes hit

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
     resources :users, only: [:index, :create, :show]
     post "/login", to: "users#login"
     get "/auto_login", to: "users#auto_login"
-
   end
   
 end
