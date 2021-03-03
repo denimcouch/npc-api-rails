bd2e578ef7172326dc709da9468068a4cf3fc18e
denimcouch <alexmata92@ymail.com>
2021-03-03 17:29:22 -0600

    post request works

--- app/controllers/api/characters_controller.rb
+++ app/controllers/api/characters_controller.rb
@@ -1,3 +1,18 @@
 class Api::CharactersController < ApplicationController
-  before_action :authorized, only: [:create]
+  before_action :authorized
+
+  def create
+    user = User.find_by(id: character_params[:user_id])
+    character = Character.new(name: character_params[:name], race: character_params[:race], is_adventurer: character_params[:is_adventurer], role: character_params[:role], user_id: character_params[:user_id])
+    # byebug
+    character.save
+    render json: {user: UserSerializer.new(user)}
+  end
+
+  private
+
+  def character_params
+    params.require(:character).permit(:name, :race, :is_adventurer, :role, :user_id)
+  end
+
 end
