14790bac7277234f0ef0ea241e76de828dfdbd46
denimcouch <alexmata92@ymail.com>
2021-03-04 16:25:38 -0600

    mvp 1.0

--- app/controllers/api/characters_controller.rb
+++ app/controllers/api/characters_controller.rb
@@ -4,7 +4,6 @@ class Api::CharactersController < ApplicationController
   def create
     user = User.find_by(id: character_params[:user_id])
     character = Character.new(name: character_params[:name], race: character_params[:race], is_adventurer: character_params[:is_adventurer], role: character_params[:role], user_id: character_params[:user_id])
-    # byebug
     character.save
     render json: {user: UserSerializer.new(user)}
   end
